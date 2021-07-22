f84be7ffb3d323510a245b4b18dba192b32568f9
realdashiedash <31542077+dashiedash@users.noreply.github.com>
2021-07-22 16:32:14 +0800

    Add new possible advices.
    I added 21 new possible sentences. There are now up to 56 combinations of weird advices.

--- obscureAdviceGenerator.js
+++ obscureAdviceGenerator.js
@@ -5,16 +5,77 @@ const getDate = () => {
    return `Today is ${day[rawDate.getDay()]}, ${rawDate.getDate()} ${month[rawDate.getMonth()]} ${rawDate.getFullYear()}.`
 }
 
-const rand = array => Math.floor(Math.random() * array.length)
+const rand = array => Math.floor(Math.random() * array.length);
 
 const getAdvice = () => {
-   verb = ["see", "witness", "eat", "drink", "write", "read", "watch"];
+   const verb = [
+      "see",
+      "witness",
+      "eat",
+      "drink",
+      "write",
+      "read",
+      "watch",
+      "play with",
+      "burn",
+      "feed",
+      "kiss",
+      "slap",
+      "confess to",
+      "cook"
+   ];
 
-   nounObject = ["cookies", "a cup of coffee", "a bowl of fried rice", "your ex", "your friend", "a book by Aristotle", "an episode of SpongeBob"];
+   const nounObject = [
+      "cookies",
+      "a cup of coffee",
+      "a bowl of fried rice",
+      "your ex",
+      "your friend",
+      "a book by Aristotle",
+      "an episode of SpongeBob",
+      "your phone",
+      "your PC",
+      "your parents",
+      "your dad",
+      "your mom",
+      "your brother",
+      "your sister",
+   ];
 
-   nounStatus = ["rolling around", "outside your house", "in your bedroom", "on the table", "flying in mid-air", "on your TV", "on a plate", "eating your food"];
+   const nounStatus = [
+      "rolling around",
+      "outside your house",
+      "in your bedroom",
+      "on the table",
+      "flying in mid-air",
+      "on your TV",
+      "on a plate",
+      "eating your food",
+      "watching anime",
+      "writing a book",
+      "lying on the ground",
+      "playing Minecraft",
+      "smashing your Xbox",
+      "doing nothing",
+      "in the bathroom"
+   ];
 
-   response = ["go to church", "sell everything you have", "eat them", "move to Japan", "say goodbye to your family and friends", "brag it on social media", "go back to sleep"];
+   const response = [
+      "go to church",
+      "sell everything you have",
+      "eat them",
+      "move to Japan",
+      "say goodbye to your family and friends",
+      "brag it on social media",
+      "go back to sleep",
+      "burn them",
+      "go shopping",
+      "learn programming",
+      "get a new one",
+      "bake a cake",
+      "congratulate them",
+      "throw a party"
+   ];
 
    return `When you ${verb[rand(verb)]} ${nounObject[rand(nounObject)]} ${nounStatus[rand(nounStatus)]}, you should ${response[rand(response)]}.`
 }
